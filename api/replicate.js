68f5312c44064228d83fa2658b72517dc7718a23
Flávio Meira <flavio.meira@luizalabs.com>
2025-03-31 15:41:38 -0300

    Adiciona suporte a CORS e trata requisições OPTIONS na função 'api/replicate.js'

--- api/replicate.js
+++ api/replicate.js
@@ -1,8 +1,18 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 module.exports = async (req, res) => {
+  // Libera CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Trata requisições OPTIONS (preflight)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed');
+    return res.status(405).send('Método não permitido');
   }
 
   const replicateApiToken = process.env.REPLICATE_API_TOKEN;
