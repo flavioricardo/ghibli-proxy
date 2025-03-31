d9359a74cca2e733b1b5f6889f79f10e92e6b6a3
Flávio Meira <flavio.meira@luizalabs.com>
2025-03-31 20:30:43 -0300

    Altera a importação do 'node-fetch' para a versão 2 e ajusta o arquivo 'package-lock.json' para refletir a mudança de dependência

--- api/replicate.js
+++ api/replicate.js
@@ -1,5 +1,4 @@
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
   const allowedOrigins = ["https://flavioricardo.github.io"];
