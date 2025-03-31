1254fcd1da01e236698aa5313cea092571100036
Flávio Meira <flavio.meira@luizalabs.com>
2025-03-31 19:51:52 -0300

    Melhora o suporte a CORS e tratamento de requisições OPTIONS na função 'api/replicate.js'; adiciona verificação de erros na resposta da API Replicate

--- api/replicate.js
+++ api/replicate.js
@@ -4,32 +4,66 @@ const fetch = (...args) =>
 module.exports = async (req, res) => {
   const allowedOrigins = ["https://flavioricardo.github.io"];
   const origin = req.headers.origin;
+
+  // Configuração de CORS
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "*");
   }
 
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
+  // Resposta para requisições OPTIONS
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
+  // Verificação do método HTTP
   if (req.method !== "POST") {
     return res.status(405).send("Método não permitido");
   }
 
+  // Token da API Replicate
   const replicateApiToken = process.env.REPLICATE_API_TOKEN;
+  if (!replicateApiToken) {
+    return res.status(500).send("Token da API Replicate não configurado");
+  }
 
-  const replicateRes = await fetch("https://api.replicate.com/v1/predictions", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${replicateApiToken}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(req.body),
-  });
+  try {
+    // Requisição para a API da Replicate
+    const replicateRes = await fetch(
+      "https://api.replicate.com/v1/predictions",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Token ${replicateApiToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body),
+      }
+    );
 
-  const data = await replicateRes.json();
-  res.status(200).json(data);
+    const data = await replicateRes.json();
+
+    // Verifica se a resposta da API contém erro
+    if (!replicateRes.ok) {
+      return res.status(replicateRes.status).json({
+        error: data.error || "Erro ao processar a requisição na API Replicate",
+      });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Erro ao se comunicar com a API Replicate:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
 };
